fe9ff4c82b989e37d34cc0810f92c8741d79719d
SM-Nasir-Uddin_Shiplu <nasiruddinshiplu@gmail.com>
2025-02-12 14:02:44 +0600

    fixing hero section height for tablet

--- src/components/HeaderSection/HeaderSection.js
+++ src/components/HeaderSection/HeaderSection.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 import { Nav } from "./components/Nav";
 import { Hero } from "./components/Hero";
 import { ScrollButton } from "./components/ScrollButton";
@@ -6,8 +6,18 @@ import { ParticleAnimation } from "./components/ParticleAnimation";
 
 export const HeaderSection = () => {
   const headerSecRef = useRef(null);
+  useEffect(() => {
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  }, []); //eslint-disable-line
+
   return (
-    <header ref={headerSecRef} className="hero h-svh flex flex-col justify-between p-4 scroll-smooth relative bg-slate-900 text-white">
+    <header 
+      ref={headerSecRef} 
+      style={{height: "100vh", height: "calc(var(--vh, 1vh) * 100)"}}
+      className="hero flex flex-col justify-between p-4 scroll-smooth relative bg-slate-900 text-white">
       <Nav/>
       <Hero />
       <ScrollButton forwardedRef={headerSecRef} />
