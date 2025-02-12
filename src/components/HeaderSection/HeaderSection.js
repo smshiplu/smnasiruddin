bd8883b85323d26db772193401bd3d8c6fe34a7f
SM-Nasir-Uddin_Shiplu <nasiruddinshiplu@gmail.com>
2025-02-12 13:19:02 +0600

    fixing header height for tab scfeen

--- src/components/HeaderSection/HeaderSection.js
+++ src/components/HeaderSection/HeaderSection.js
@@ -7,7 +7,7 @@ import { ParticleAnimation } from "./components/ParticleAnimation";
 export const HeaderSection = () => {
   const headerSecRef = useRef(null);
   return (
-    <header ref={headerSecRef} className="hero h-dvh flex flex-col justify-between p-4 scroll-smooth relative bg-slate-900 text-white">
+    <header ref={headerSecRef} className="hero h-screen flex flex-col justify-between p-4 scroll-smooth relative bg-slate-900 text-white">
       <Nav/>
       <Hero />
       <ScrollButton forwardedRef={headerSecRef} />
