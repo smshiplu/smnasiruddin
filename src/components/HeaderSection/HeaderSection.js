2dad3155d054215ccb6994aec777831c782cc9ed
SM-Nasir-Uddin_Shiplu <nasiruddinshiplu@gmail.com>
2026-06-07 17:55:09 +0600

    fixing header component heght

--- src/components/HeaderSection/HeaderSection.js
+++ src/components/HeaderSection/HeaderSection.js
@@ -8,16 +8,22 @@ export const HeaderSection = () => {
   
   const headerSecRef = useRef(null);
   useEffect(() => {
-    //https://css-tricks.com/the-trick-to-viewport-units-on-mobile
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    function fixHeight() {
+      //https://css-tricks.com/the-trick-to-viewport-units-on-mobile
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    }
+
+    window.addEventListener('resize', fixHeight);
+
+    window.removeEventListener('resize', fixHeight);
   }, []); //eslint-disable-line
 
   return (
     <header 
       ref={headerSecRef} 
       style={{ height: "100vh", height: "calc(var(--vh, 1vh) * 100)"}} //eslint-disable-line
-      className="header flex flex-col justify-between p-4 scroll-smooth relative bg-slate-900 text-white h-dvh">
+      className="header flex flex-col justify-between p-4 scroll-smooth relative bg-slate-900 text-white h-screen">
       <Nav/>
       <Hero />
       <ScrollButton forwardedRef={headerSecRef} />
